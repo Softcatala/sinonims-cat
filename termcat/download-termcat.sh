d47771e3ba0f611d7f8cadf0579bd67a73a634f6
jaumeortola <jaumeortola@gmail.com>
2021-05-13 19:40:21 +0200

    extract termcat

--- termcat/download-termcat.sh
+++ termcat/download-termcat.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 rm -rf xml-dicts
 mkdir xml-dicts
 cd xml-dicts
@@ -7,6 +6,12 @@ for i in {0..15}
 do
 	wget https://www.termcat.cat/ca/terminologia-oberta?page=${i}
 done
-grep 'href=".*.xml"' terminologia* | sed -E 's/.*href=\"(.*\.xml)\".*/\1/' > termcat-dicts.txt
+rm termcat-dicts.txt
+rm area-dicts.txt
+for i in {0..15}
+do
+	grep 'href=".*.xml"' terminologia*page=${i} | sed -E 's/.*href=\"(.*\.xml)\".*/\1/' >> termcat-dicts.txt
+	grep 'class="service-item__tag--main"' terminologia*page=${i} | sed -E 's/.*>&lt;(.*)&gt;<.*/\1/' >> area-dicts.txt 
+done
 rm terminologia-oberta*
 wget -i termcat-dicts.txt
---
+++ termcat/extract-syn-termcat.sh
@@ -0,0 +1,2 @@
+python3 extract-syn-termcat.py > termcat.txt
+sort -u termcat.txt -o termcat2.txt 
