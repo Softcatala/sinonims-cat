be39920d28464475e0709880feb11bea2d9e19ff
jaumeortola <jaumeortola@gmail.com>
2023-02-05 23:09:34 +0100

    dict update

--- server/src/main/java/org/softcatala/sinonims/Dictionary.java
+++ server/src/main/java/org/softcatala/sinonims/Dictionary.java
@@ -248,7 +248,7 @@ public class Dictionary {
       "ecogastronomia", "slow food", "no tindre un qüe", "cloroformitzador", "a bacs i redolons", "de bòbilis-bòbilis",
       "menjaclosques", "imbarrejable", "opinaire", "storytelling", "torcaboquer", "portatovallons", "tovallonera",
       "de bocons", "entrepussar", "lliberticidi", "camperitzar", "camperització", "rojoncós", "emmetxament",
-      "ginoide", "encadellament", "escrache", "ITS", "girajaquetes", "polímata" });
+      "ginoide", "encadellament", "escrache", "ITS", "girajaquetes", "polímata", "ual·la"});
 
   Dictionary(ThesaurusConfig configuration) throws IOException {
 
