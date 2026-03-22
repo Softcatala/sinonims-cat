b8d57160abf3b0d3f03e7c886d764dda5bff91e9
jaumeortola <jaumeortola@gmail.com>
2026-03-22 20:24:25 +0100

    dict update: bonyegut

--- server/src/main/java/org/softcatala/sinonims/Dictionary.java
+++ server/src/main/java/org/softcatala/sinonims/Dictionary.java
@@ -281,7 +281,7 @@ public class Dictionary {
       "a bocafoscant", "semifalla", "brugar", "superordinat", "superordinació", "topaló", "nikkei", "ressenyista",
       "schorre", "lladoncs", "lluciferisme", "cagueta", "pentura", "posteditor", "bujolada", "pèmpins", "frisbi", "cd",
       "gamaleu", "safranòria", "forquillada", "mossar", "buldroi", "llauseta", "bosníac", "escarrufador", "priàpic",
-      "amagable", "boscarró", "lleterosa", "espòlits", "cri-cri", "violinaire", "gatillop", "comunot");
+      "amagable", "boscarró", "lleterosa", "espòlits", "cri-cri", "violinaire", "gatillop", "comunot", "boni");
 
   Dictionary(ThesaurusConfig configuration) throws IOException {
 
